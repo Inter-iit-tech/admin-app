bd455cba86d2880c295c5d2b97acf5ece3dc103f
Kartikeya <pk39@iitbbs.ac.in>
2023-01-14 01:54:24 +0530

    Added order screen

---
+++ components/horizontalCard.js
@@ -0,0 +1,91 @@
+import { StyleSheet, Text, View, Image, ScrollView } from "react-native";
+import food from "./../assets/images/food.png";
+import { Icon } from "@rneui/themed";
+
+const HorizontalCard = ({ order }) => {
+  return (
+    <View style={styles.orderCard}>
+      <Image source={food} style={styles.image} />
+      <View style={styles.content}>
+        <Text style={styles.title}>{order.customer}</Text>
+        <View style={styles.subContent}>
+          <View style={styles.address}>
+            <Icon
+              name="location-outline"
+              type="ionicon"
+              containerStyle={styles.icon}
+              size={18}
+            />
+            <Text style={styles.text}>{order.address}</Text>
+          </View>
+          <View style={styles.address}>
+            <Icon name="date-range" containerStyle={styles.icon} size={18} />
+            <Text style={styles.text}>{order.date}</Text>
+          </View>
+        </View>
+        <View style={styles.statusContainer}>
+          <Text style={styles.status}>{order.status}</Text>
+        </View>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  orderCard: {
+    backgroundColor: "white",
+    borderRadius: 20,
+    marginTop: 10,
+    marginBottom: 10,
+    padding: 15,
+    display: "flex",
+    justifyContent: "flex-start",
+    flexDirection: "row",
+    minHeight: 140,
+  },
+  image: {
+    height: "100%",
+    width: 120,
+    borderRadius: 20,
+  },
+  content: {
+    flexGrow: 1,
+    height: "100%",
+    paddingHorizontal: 20,
+    display: "flex",
+    flexDirection: "column",
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: "600",
+    color: "#077a87",
+  },
+  status: {
+    marginTop: "auto",
+    width: 100,
+    paddingVertical: 4,
+    backgroundColor: "#87CEEB",
+    borderRadius: 20,
+    textAlign: "center",
+    color: "white",
+  },
+  address: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  subContent: {
+    marginVertical: 5,
+    flexGrow: 1,
+    display: "flex",
+    justifyContent: "space-evenly",
+  },
+  icon: {
+    marginRight: 5,
+  },
+  statusContainer: { alignItems: "flex-end" },
+  text: {
+    fontSize: 12,
+  },
+});
+export default HorizontalCard;
--- screens/Orders.js
+++ screens/Orders.js
@@ -1,11 +1,44 @@
 import { useState } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, ScrollView } from "react-native";
 import { SearchBar } from "@rneui/themed";
+import food from "./../assets/images/food.png";
+import HorizontalCard from "../components/horizontalCard";
 
+const orders = [
+  {
+    image: food,
+    customer: "David",
+    address: "Hyderabad, Telangana",
+    date: "18 june, 2023",
+    status: "Delivered",
+  },
+  {
+    image: food,
+    customer: "Tim",
+    address: "Hyderabad, Telangana",
+    date: "18 june, 2023",
+    status: "Delivered",
+  },
+  {
+    image: food,
+    customer: "Johns",
+    address: "Hyderabad, Telangana",
+    date: "18 june, 2023",
+    status: "Delivered",
+  },
+  {
+    image: food,
+    customer: "David",
+    address: "Hyderabad, Telangana",
+    date: "18 june, 2023",
+    status: "Delivered",
+  },
+];
 export default function Orders() {
   const [search, setSearch] = useState("");
 
   const updateSearch = (search) => {
+    console.log(search);
     setSearch(search);
   };
 
@@ -15,12 +48,17 @@ export default function Orders() {
         <SearchBar
           value={search}
           onChangeText={updateSearch}
-          placeholder="Type Here..."
+          placeholder="Search"
           lightTheme={true}
           containerStyle={styles.searchContainer}
           inputContainerStyle={styles.inputContainer}
         />
       </View>
+      <ScrollView style={styles.orders}>
+        {orders.map((order, index) => (
+          <HorizontalCard order={order} key={index} />
+        ))}
+      </ScrollView>
     </View>
   );
 }
@@ -29,16 +67,28 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     flexDirection: "column",
-    margin: 15,
   },
-  searchBar: {},
-  searchContainer: {
+  searchBar: {
+    justifyContent: "center",
+    alignItems: "center",
+    borderWidth: 0,
     backgroundColor: "white",
+    paddingBottom: 20,
+  },
+  searchContainer: {
+    width: 350,
+    padding: 0,
     borderTopWidth: 0,
     borderBottomWidth: 0,
   },
   inputContainer: {
-    backgroundColor: "white",
-    height: 30,
+    backgroundColor: "#F1F3F4",
+    margin: 0,
+    borderWidth: 0,
+  },
+  orders: {
+    height: "100%",
+    padding: 20,
+    marginBottom: 10,
   },
 });
