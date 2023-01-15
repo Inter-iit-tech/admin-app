f5ee72cf8daf8f8218799bbd45641085a982dffb
Shrirang Deshmukh <deshmukhshrirang01@gmail.com>
2023-01-15 15:42:15 +0530

    Added Rider Cards

---
+++ components/RiderCard.js
@@ -0,0 +1,101 @@
+import { StyleSheet, Text, View } from "react-native";
+import { useState } from "react";
+import { ListItem, Avatar, Divider } from "@rneui/themed";
+
+const RiderCard = ({ rider }) => {
+  const [expanded, setExpanded] = useState(false);
+
+  return (
+    <ListItem.Accordion
+      bottomDivider
+      topDivider
+      content={
+        <>
+          <Avatar
+            rounded
+            source={{
+              uri: "https://randomuser.me/api/portraits/men/36.jpg",
+            }}
+          />
+          <ListItem.Content style={styles.title}>
+            <Text>{rider.name}</Text>
+            <Text>{rider.id}</Text>
+          </ListItem.Content>
+        </>
+      }
+      isExpanded={expanded}
+      onPress={() => {
+        setExpanded(!expanded);
+      }}
+    >
+      {
+        <ListItem topDivider bottomDivider style={styles.content}>
+          <ListItem.Content>
+            <View style={styles.address}>
+              <Text style={styles.type}>Total Bag Volume:</Text>
+              <Text style={styles.text}>{rider.totalBagVolume} Litres</Text>
+            </View>
+
+            <View style={styles.address}>
+              <Text style={styles.type}>Current Available Bag Volume:</Text>
+              <Text style={styles.text}>
+                {rider.currentAvailableBagVolume} Litres
+              </Text>
+            </View>
+
+            <View style={styles.address}>
+              <Text style={styles.type}>Next Delivery Location:</Text>
+              <Text style={styles.text}>{rider.nextDeliveryLocation}</Text>
+            </View>
+          </ListItem.Content>
+        </ListItem>
+      }
+    </ListItem.Accordion>
+  );
+};
+
+const styles = StyleSheet.create({
+  orderCard: {
+    backgroundColor: "white",
+    borderRadius: 20,
+    marginTop: 10,
+    marginBottom: 10,
+    padding: 15,
+    display: "flex",
+    justifyContent: "flex-start",
+    flexDirection: "row",
+    minHeight: 130,
+  },
+  subContent: {
+    marginVertical: 5,
+    flexGrow: 1,
+    display: "flex",
+    justifyContent: "space-evenly",
+  },
+  icon: {
+    marginRight: 5,
+  },
+  statusContainer: { alignItems: "flex-end" },
+
+  address: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    flexWrap: "wrap",
+  },
+  text: {
+    fontSize: 12,
+  },
+  type: {
+    fontWeight: "bold",
+    fontSize: 13,
+    marginRight: 7,
+  },
+  title: {
+    marginLeft: "5%",
+  },
+  content: {
+    marginBottom: "5%",
+  },
+});
+export default RiderCard;
--- screens/Riders.js
+++ screens/Riders.js
@@ -1,9 +1,57 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, ScrollView } from "react-native";
+import RiderCard from "../components/RiderCard";
+
+const dummyRiders = [
+  {
+    name: "John Smith",
+    id: "1234567890",
+    totalBagVolume: 45.67,
+    currentAvailableBagVolume: 23.56,
+    nextDeliveryLocation: "6, Shakambari Nagar, 1st stage, JP Nagar, Bangalore",
+    ordersAssignedToday: 20,
+  },
+  {
+    name: "John Doe",
+    id: "123456676s7",
+    totalBagVolume: 45.67,
+    currentAvailableBagVolume: 23.56,
+    nextDeliveryLocation: "6, Shakambari Nagar, 1st stage, JP Nagar, Bangalore",
+    ordersAssignedToday: 20,
+  },
+  {
+    name: "John Doe",
+    id: "123456798989",
+    totalBagVolume: 45.67,
+    currentAvailableBagVolume: 23.56,
+    nextDeliveryLocation: "6, Shakambari Nagar, 1st stage, JP Nagar, Bangalore",
+    ordersAssignedToday: 20,
+  },
+  {
+    name: "John Doe",
+    id: "12345679090",
+    totalBagVolume: 45.67,
+    currentAvailableBagVolume: 23.56,
+    nextDeliveryLocation: "6, Shakambari Nagar, 1st stage, JP Nagar, Bangalore",
+    ordersAssignedToday: 20,
+  },
+  {
+    name: "John Doe",
+    id: "1234567909",
+    totalBagVolume: 45.67,
+    currentAvailableBagVolume: 23.56,
+    nextDeliveryLocation: "6, Shakambari Nagar, 1st stage, JP Nagar, Bangalore",
+    ordersAssignedToday: 20,
+  },
+];
 
 export default function Riders() {
   return (
     <View style={styles.container}>
-      <Text>Riders</Text>
+      <ScrollView style={styles.orders}>
+        {dummyRiders.map((rider, index) => (
+          <RiderCard rider={rider} key={index} />
+        ))}
+      </ScrollView>
     </View>
   );
 }
@@ -11,8 +59,24 @@ export default function Riders() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+  },
+  orders: {
+    height: "100%",
+    padding: 20,
+    marginBottom: 10,
+  },
+  address: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    flexWrap: "wrap",
+  },
+  text: {
+    fontSize: 12,
+  },
+  type: {
+    fontWeight: "bold",
+    fontSize: 13,
+    marginRight: 7,
   },
 });
