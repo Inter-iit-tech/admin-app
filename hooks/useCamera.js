21b3552bd3e04eaff12f64009b0f679c00f3e50d
PranayB003 <pranayborgohain@gmail.com>
2023-02-04 11:57:29 +0530

    added: camera and media permission hooks

---
+++ hooks/useCamera.js
@@ -0,0 +1,26 @@
+import { useCameraPermissions, PermissionStatus } from "expo-image-picker";
+
+const useCamera = () => {
+  const [cameraPermissionStatus, requestCameraPermission] =
+    useCameraPermissions();
+
+  const getCameraPermission = async () => {
+    if (cameraPermissionStatus?.status === PermissionStatus.UNDETERMINED) {
+      const permissionResponse = await requestCameraPermission();
+      return permissionResponse.granted;
+    } else if (cameraPermissionStatus?.status === PermissionStatus.DENIED) {
+      Alert.alert(
+        "Insufficient Permissions",
+        "Please grant access to the device camera to click images."
+      );
+      cameraPermissionStatus.status = PermissionStatus.UNDETERMINED;
+      const res = await getCameraPermission();
+      return res;
+    }
+    return true;
+  };
+
+  return getCameraPermission;
+};
+
+export default useCamera;
