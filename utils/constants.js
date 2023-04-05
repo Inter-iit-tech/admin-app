f7c42fb38194b45b92d4db0b607ac04b48b70549
Sarthak <sg60@iitbbs.ac.in>
2023-04-06 02:10:41 +0530

    changed backend url to hosted version

--- utils/constants.js
+++ utils/constants.js
@@ -1,3 +1,3 @@
-export const MAIN_SERVER_URL = "http://10.10.28.114:3000";
+export const MAIN_SERVER_URL = "https://main-backend-rbb2mcozsa-em.a.run.app";
 export const VOLUME_ESTIMATION_SERVICE_URL =
   "https://volumetric-rbb2mcozsa-em.a.run.app";
